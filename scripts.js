23f8729e32082791142b953f7ef49aad1dfc82a9
Dylan Chew <dylanw.chew@gmail.com>
2018-11-22 21:26:00 -0400

    setting up new repo for web

---
+++ scripts.js
@@ -0,0 +1,11 @@
+"use strict"
+
+function navButton() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } 
+    else {
+        x.className = "topnav";
+    }
+}
