283372ec1ab64a6b20b9d663a0e186a29b70772d
Dylan Chew <dylanw.chew@gmail.com>
2018-11-29 21:45:24 -0400

    working on goals page

--- Portfolio/scripts.js
+++ Portfolio/scripts.js
@@ -9,3 +9,34 @@ function navButton() {
         x.className = "topnav";
     }
 }
+
+/* Flipper Left */
+
+function flipCardLeft()
+{
+    document.getElementById('headerLeft').style.display = 'none';
+    document.getElementById('ulLeft').style.display = 'block';}
+
+function returnCardLeft()
+{
+    document.getElementById('headerLeft').style.display = 'block';
+    document.getElementById('ulLeft').style.display = 'none';
+}
+
+
+
+
+/* Flipper Right */
+
+
+function flipCardRight()
+{
+    document.getElementById('headerRight').style.display = 'none';
+    document.getElementById('ulRight').style.display = 'block';
+}
+
+function returnCardRight()
+{
+    document.getElementById('headerRight').style.display = 'block';
+    document.getElementById('ulRight').style.display = 'none';
+}
